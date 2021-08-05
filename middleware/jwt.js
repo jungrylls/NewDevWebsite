20b321c4cfecadd47d4dcef32a59b04926054387
jungrylls <51180901+jungrylls@users.noreply.github.com>
2021-08-05 15:20:10 -0700

    style on posts.ejs

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,21 +1,30 @@
+//Import jwtwebtoken
 let jwt = require('jsonwebtoken');
+
+//Import cookie parser.
 const cookieParser = require("cookie-parser");
-//.env import
+
+//import .env
 require('dotenv').config();
 
+//Check if the user is logged in
 let checkToken = (req, res, next) => {
+
+    //Retrieve the jwt token from the browser cookie.
     const token = req.cookies.jwttoken;
 
+    //If no valid token, then redirect to homepage.
     if (token === undefined) {
         return res.redirect('/');
-    } else {
+    }
+    //If valid token, then retrieve user data and proceed to next();
+    else {
         const obj = JSON.parse(token);
         try {
             const user = jwt.verify(obj.token, process.env.secret);
             req.user = user;
             next();
         } catch (err) {
-            //res.clearCookie("jwttoken");
             return res.redirect('/');
 
         }
@@ -23,6 +32,7 @@ let checkToken = (req, res, next) => {
 
 }
 
+//Export module.
 module.exports = {
     checkToken: checkToken
 }
