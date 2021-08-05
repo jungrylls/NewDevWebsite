a4ddd9e13629b35e99022020d40f6d11f8d8c3b0
jungrylls <51180901+jungrylls@users.noreply.github.com>
2021-08-05 11:56:14 -0700

    Search.js
    When searched, it correctly displays matched elements.
    Pagination for search added.

--- routes/search.js
+++ routes/search.js
@@ -3,9 +3,13 @@ const router = express();
 const pool = require('../config/database');
 
 
-router.post('/search', (req, res) => {                                                          //
+router.post('/search:page', (req, res) => {                                                          //
                                                                                                     
-    //Get pagination number.   
+    //Get pagination number.                                                                        //
+    const page = req.params.page;                                                                   
+    //Get offset value for mySQL pagination query.                                                  //
+    const offSet = (page * 5) - 5;     
+
     console.log(req.body)    
     const {SearchInput} = req.body;                                                                  //                                             
     console.log(SearchInput)                                                        
@@ -16,7 +20,10 @@ router.post('/search', (req, res) => {                                          
             throw err;
         }      
     var insert = ['user_post', 'title'];                                                                 
-    var sqlQuery = `SELECT * FROM ?? WHERE ?? LIKE '%${SearchInput}%'`; 
+    var sqlQuery = `SELECT * FROM ?? LIMIT ${offSet}, 5 WHERE ?? LIKE '%${SearchInput}%'`; 
+
+    var insert2 = ['user_post', 'title'];
+    var sqlQuery2 = `SELECT count(*) as count FROM ?? WHERE ?? LIKE '%${SearchInput}%'`;
         
         conn.query(sqlQuery, insert,  (err, matchedPosts) => {
             if(err){
@@ -24,17 +31,29 @@ router.post('/search', (req, res) => {                                          
                 throw err;
             }
 
-        //Get jwt token from the cookie.
-        const token = req.cookies.jwttoken;
-        const user  = JSON.parse(token);
-        //Data to be sent to the edit.EJS.
-         const data = {
-             SearchInput,
-             matchedPosts,
-             user
-         }
-         console.log(data)
-         res.render('search', {data:data})
+            conn.query(sqlQuery2, insert2, (err, numbOfRows) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+
+                const numb = JSON.parse(JSON.stringify(numbOfRows))
+
+                console.log("the number of matched posts" + numb)
+                 //Get jwt token from the cookie.
+                const token = req.cookies.jwttoken;
+                const user  = JSON.parse(token);
+                //Data to be sent to the edit.EJS.
+                 const data = {
+                     SearchInput,
+                     matchedPosts,
+                     user,
+                     numb
+                 }
+                console.log(data)
+                 res.render('search', {data:data})
+
+            })
         })
 
     })  
