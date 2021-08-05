cab106feb9bace306866912b30b4247bc351ee34
jungrylls <51180901+jungrylls@users.noreply.github.com>
2021-08-05 10:27:30 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,17 +4,17 @@ var pool  = mysql.createPool({
     connectTimeout  : 60 * 60 * 1000,
     acquireTimeout  : 60 * 60 * 1000,
     timeout         : 60 * 60 * 1000,
-  host            : 'localhost',
-  user            : 'root',
-  password        : 'acAC12!@',
-  database        : 'posts',
+  host            : process.env.HOST,
+  user            : process.env.USER,
+  password        : process.env.PASSWORD,
+  database        : process.env.DATABASE,
   port: '3306',
   multipleStatements: true
 });
 
 pool.getConnection((err, connection) => {
     if(err){
-        console.log("hello there")
+        console.log("data base connection error !")
         console.log(err)
     }else{
         console.log("connected to the pool !")
